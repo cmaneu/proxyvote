9bfbebda15640206e112be997ac1d350fdff41bb
Christopher MANEU <git@maneu.org>
2022-11-04 15:28:38 +0100

    [test-data]: Revamp data generation

--- test-data/TestDataGenerator/Program.cs
+++ test-data/TestDataGenerator/Program.cs
@@ -1,29 +1,24 @@
-﻿using Bogus;using Bogus.DataSets;
-
+﻿using Bogus;
+using Bogus.DataSets;
 using CountryData;
 using CountryData.Bogus;
-
+using Spectre.Console;
 using Microsoft.Azure.Cosmos;
-
+using System.Linq;
 using ProxyVote.Core.Entities;
 using ProxyVote.IdentityAuthority.Core;
-
 using TestDataGenerator;
 
-Console.WriteLine("ProxyVote - Data Generator");
+AnsiConsole.Write(
+    new FigletText("ProxyVote")
+        .LeftAligned()
+        .Color(Color.Blue));
+AnsiConsole.MarkupLine("[bold]ProxyVote Test Data Generator[/]");
 
 // Setup Bogus
 var locale = args.Length >= 3 ? args[2] : "fr"; 
 var names = new Bogus.DataSets.Name(locale);
-
-
-
 var addresses = new Bogus.DataSets.Address(locale);
-
-
-
-
-
 
 // Setup fakers
 var fakeProxy = new Faker<ProxyVoter>(locale)
@@ -80,27 +75,113 @@ RegistrationIdentityService registrationIdentityService = new RegistrationIdenti
 
 var random = new Random();
 
+var numberOfApplicationsToGenerate = args.Count() >= 4 ? Convert.ToInt32(args[3]) : 10000;
 
-for (int i = 0; i < 100000; i++)
-{
-    var fakeItem = fakeRegistration.Generate();
-    ItemResponse<TestProxyApplication> createResponse = await container.CreateItemAsync(fakeItem);
+var batchSize = 100;
+var percentageOfRegistrationsToValidate= 2;
 
-    // Validate some applications
-    if (i % 100 == 1)
+// Create application with batch of 100 items size
+// Save some of them for validation
+
+AnsiConsole.MarkupLineInterpolated($"[blue]Generating {numberOfApplicationsToGenerate} applications[/]");
+
+// Asynchronous
+
+    await AnsiConsole.Progress()
+    .Columns(new ProgressColumn[]
     {
-        Console.WriteLine(i);
-        var validation = new ApplicationValidation()
+        new TaskDescriptionColumn(),
+        new ProgressBarColumn(),
+        new PercentageColumn(),
+        new RemainingTimeColumn(),
+        new SpinnerColumn(Spinner.Known.Earth)
+    })
+    .StartAsync(async ctx =>
+    {
+        // Define tasks
+        var task1 = ctx.AddTask("[green]Global progress:[/]");
+        task1.MaxValue = numberOfApplicationsToGenerate;
+        var task2 = ctx.AddTask("      [blue]Generating items[/]");
+        var task3 = ctx.AddTask("      [blue]Uploading new applications to Cosmos[/]");
+        var task4 = ctx.AddTask("      [blue]Validating registrations[/]");
+
+
+        // One batch
+        for(int i = 0; i < numberOfApplicationsToGenerate; i+= batchSize)
         {
-            Type = "test",
-            ValidationData = "na",
-            ValidationTime = fakeItem.CreatedAt.Value.AddDays(1).AddDays(random.Next(0, 15)),
-            ValidatorId = random.Next(10000, 12000).ToString()
-        };
+            List<double> consumedRU = new List<double>(batchSize);
+            task2.Value = 0;
+            task3.Value = 0;
+            task4.Value = 0;
 
-        await registrationIdentityService.ValidateRegistration("2022-" + fakeItem.Applicant.PostalCode[..2],
-            fakeItem.RegistrationId, validation);
 
-    }
+            // Generate batchSize items
+            var fakeRegistrations = fakeRegistration.Generate(batchSize).ToList();
+            // Pick random items to validate
+            var fakeRegistrationToValidate = fakeRegistrations.OrderBy(x => random.Next()).Take(10).ToList();
+            task2.Value = 100;
 
-}
+            // Send them to Cosmos
+            task3.MaxValue = batchSize;
+            foreach (var reg in fakeRegistrations)
+            {
+                ItemResponse<TestProxyApplication> createResponse = await container.CreateItemAsync(reg);
+                consumedRU.Add(createResponse.RequestCharge);
+                task3.Increment(1);
+            }
+
+             AnsiConsole.MarkupLineInterpolated ($"Average Consumed RU: {consumedRU.Average().ToString()}");
+            
+
+            // Validate items
+            task4.MaxValue = fakeRegistrationToValidate.Count();
+            AnsiConsole.MarkupLineInterpolated($"[blue]Validating {fakeRegistrationToValidate.Count()} registrations[/]");
+            foreach (var reg in fakeRegistrationToValidate)
+            {
+                var validation = new ApplicationValidation()
+                {
+                    Type = "test",
+                    ValidationData = "na",
+                    ValidationTime = reg.CreatedAt.Value.AddDays(1).AddDays(random.Next(0, 15)),
+                    ValidatorId = random.Next(10000, 12000).ToString()
+                };
+
+                await registrationIdentityService.ValidateRegistration("2022-" + reg.Applicant.PostalCode[..2],
+                    reg.RegistrationId, validation);
+                task4.Increment(1);
+            }
+            fakeRegistrationToValidate.DisplayAsConsoleTable("Applications validated");
+            task1.Increment(batchSize);
+        }
+    });
+       
+
+
+
+
+
+// /// Old version below
+// return;
+// for (int i = 0; i < 100000; i++)
+// {
+//     var fakeItem = fakeRegistration.Generate();
+//     ItemResponse<TestProxyApplication> createResponse = await container.CreateItemAsync(fakeItem);
+
+//     // Validate some applications
+//     if (i % 100 == 1)
+//     {
+//         Console.WriteLine(i);
+//         var validation = new ApplicationValidation()
+//         {
+//             Type = "test",
+//             ValidationData = "na",
+//             ValidationTime = fakeItem.CreatedAt.Value.AddDays(1).AddDays(random.Next(0, 15)),
+//             ValidatorId = random.Next(10000, 12000).ToString()
+//         };
+
+//         await registrationIdentityService.ValidateRegistration("2022-" + fakeItem.Applicant.PostalCode[..2],
+//             fakeItem.RegistrationId, validation);
+
+//     }
+
+// }
